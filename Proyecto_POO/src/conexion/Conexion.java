05dc9d29f1e6d5bd0f617320eb3066a366509b3b
BryanLS736 <bryanlserna07@gmail.com>
2026-06-25 22:12:41 -0500

    Modificaciones en conexion y eliminación del paquete factory

--- Proyecto_POO/src/conexion/Conexion.java
+++ Proyecto_POO/src/conexion/Conexion.java
@@ -1,46 +1,16 @@
 package conexion;
+
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
 
-    // Instancia única (Singleton)
-    private static Conexion instancia;
-
-    // Conexión a la base de datos
-    private Connection conexion;
-
-    // Configuración de la base de datos
     private static final String URL = "jdbc:mysql://localhost:3306/panaderia_db";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "1234";
 
-    // Constructor privado para evitar instanciación externa
-    private Conexion() {
-        try {
-            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(
-                "Error al conectar con la base de datos", e);
-        }
-    }
-
-    // Obtiene la única instancia de la clase
-    public static Conexion getInstancia() {
-        if (instancia == null) {
-            instancia = new Conexion();
-        }
-        return instancia;
-    }
-
-    // Devuelve una conexión válida
-    public Connection getConexion() throws SQLException {
-
-        if (conexion == null || conexion.isClosed()) {
-            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-        }
-
-        return conexion;
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
     }
 }
--- Proyecto_POO/src/proyecto_poo/Principal.java
+++ Proyecto_POO/src/proyecto_poo/Principal.java
@@ -1,10 +1,14 @@
 package proyecto_poo;
 
+import conexion.Conexion;
 import vista.area0Login.FormLogin;
+import java.sql.*;
 
 public class Principal {
 
     public static void main(String[] args) {
+        Conexion con = new Conexion();
+        
         FormLogin form = new FormLogin();
         form.setVisible(true);
         form.setLocationRelativeTo(null);
